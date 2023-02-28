f7b7714698668a165a54dfe75dac6a5d64df3d50
aishaandatt <aishaandatt2000@gmail.com>
2023-02-28 22:08:23 +0530

    resolved api call

--- src/Component/Weather.jsx
+++ src/Component/Weather.jsx
@@ -8,85 +8,84 @@ import { IconContext } from "react-icons";
 import { FaWater } from "react-icons/fa";
 import { FiWind } from "react-icons/fi";
 const Weather = () => {
-  const [weather, setWeather] = useState({});
-  const [city, setCity] = useState('Sample City');
-  const [cityP, setCityP] = useState('')
-  const [desc, setDesc] = useState('Sample Description')
-  const [humid, setHumid] = useState('00.00')
-  const [wind, setWind] = useState('00.00')
-  const [temper, setTemp] = useState('00.00')
-  const apiKey = '04917fbf4817750e09b47a725734c8ec';
-  const apiUrl = 'https://api.openweathermap.org/data/2.5/weather';
-  const handleChange = (e) => {
-    setCity(cityP)
-  }
-  const handleSubmit = event => {
-    event.preventDefault();
-    axios.get(apiUrl, {
-      params: {
-        q: city,
-        units: 'metric',
-        appid: apiKey,
-      }
-    })
-      .then(response => {
-        setWeather(response.data);
-        setDesc(weather.weather[0].description)
-        setTemp(weather.main.temp)
-        setHumid(weather.main.humidity)
-        setWind(weather.wind.speed)
-        console.log(weather)
-      })
-      .catch(error => {
-        // console.log(error);
-      });
+  const [apiData, setApiData] = useState({});
+  const [getState, setGetState] = useState('delhi');
+  const [state, setState] = useState('delhi');
+  const apiKey = '04917fbf4817750e09b47a725734c8ec'
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${state}&appid=${apiKey}`;
+
+  const inputHandler = (event) => {
+    setGetState(event.target.value);
   };
+
+  const submitHandler = () => {
+    setState(getState);
+  };
+  const kelvinToFarenheit = (k) => {
+    return (k - 273.15).toFixed(2);
+  };
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter') {
+      setState(getState)
+    }
+  }
+
+  useEffect(() => {
+    fetch(apiUrl)
+      .then((res) => res.json())
+      .then((data) => setApiData(data));
+    console.log(apiData)
+  }, [apiUrl]);
   return (
     <IconContext.Provider value={{ size: "3.4vh" }}>
-      <div className='weather'>
-        <div className="card">
-          <div className="sec0">
-            <div className='search-bar'>
-              <form onSubmit={handleSubmit}>
-                <input type='text' value={cityP}
-                  onChange={event => setCityP(event.target.value)}
-                  placeholder="Search..."
-                />
-                <button type='submit' onClick={handleChange}>
-                  <BiSearchAlt />
-                </button>
-              </form>
+      {apiData.main ? (
+
+        <div className='weather'>
+          <div className="card">
+            <div className="sec0">
+              <div className='search-bar'>
+                <div className='form'>
+                  <input type='text' value={getState}
+                    onChange={inputHandler}
+                    placeholder="Search..."
+                    onKeyDown={handleKeyDown}
+                  />
+                  <button type='submit' onClick={submitHandler}>
+                    <BiSearchAlt />
+                  </button>
+                </div>
+              </div>
             </div>
-          </div>
-          <div className="sec1">
-            <div className='location'>
-              <MdMyLocation />
-              <p>{city}</p>
-            </div>
-            {/* <div className="search">
+            <div className="sec1">
+              <div className='location'>
+                <MdMyLocation />
+                <p>{apiData.name}</p>
+              </div>
+              {/* <div className="search">
               <BiSearchAlt />
             </div> */}
-          </div>
-          <div className="sec2">
-            <img src='assets/Sunny.svg' alt='' />
-          </div>
-          <div className="sec3">
-            <div className='temp'>{temper}°</div>
-            <div className="desc">{desc}</div>
-          </div>
-          <div className="sec4">
-            <div className="humidity">
-              <FaWater />
-              <p>{humid}%</p>
             </div>
-            <div className="wind">
-              <FiWind />
-              <p>{wind} Km/h</p>
+            <div className="sec2">
+              <img src='assets/Sunny.svg' alt='' />
+            </div>
+            <div className="sec3">
+              <div className='temp'>{kelvinToFarenheit(apiData.main.temp)}°C</div>
+              <div className="desc">{apiData.weather[0].description}</div>
+            </div>
+            <div className="sec4">
+              <div className="humidity">
+                <FaWater />
+                <p>{apiData.main.humidity}%</p>
+              </div>
+              <div className="wind">
+                <FiWind />
+                <p>{apiData.wind.speed} Km/h</p>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </IconContext.Provider>
+      ) : (<h1>Loading</h1>)}
+    </IconContext.Provider >
   )
 }
 
